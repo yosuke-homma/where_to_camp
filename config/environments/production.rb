fb47cf33ca107f175c8fdda826f02a74a6bb3d38
yosuke-homma <y.homma0708@gmail.com>
2021-09-23 21:20:18 +0900

    Set SendGrid instead of Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,15 +68,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://where-to-camp.herokuapp.com'
+  config.action_mailer.perform_deliveries = true
+  host = '【Herokuのサブドメイン】.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
+    :port => 587,
+    :address => 'smtp.sendgrid.net',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => host,
     :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
